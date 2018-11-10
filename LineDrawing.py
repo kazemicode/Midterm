c29afaae740f3d5e11da9a4d12d1dc74ee2958bd
kazemicode <sarieses@gmail.com>
2018-11-10 13:55:49 -0800

    Added an alternative solution to line drawing using a lambda function
    Does not require converting image to grayscale first!

--- LineDrawing.py
+++ LineDrawing.py
@@ -38,6 +38,36 @@ def lineDrawing(pic):
         setColor(getPixel(pic, x, y), white)
   show(pic)      
   return pic
+
+def lineDrawingLambda(pic, DIFFERENCE):
+
+  # For each pixel in the picture, 
+  # compare the luminance of that pixel to the luminance of the pixels to 
+  # both the pixels below it and to the right.
+  for x in range(0, getWidth(pic) - 1):
+    for y in range(0, getHeight(pic) - 1):
+       
+      px = getPixel(pic, x, y) #pixel to change
+      right = getPixel(pic, x+1, y) #pixel to right
+      below = getPixel(pic, x, y+1) #pixel below
+      
+      #calculate the luminance of the three pixels
+      luminance = lambda px: (getRed(px) * 0.299) + (getGreen(px) * 0.587) + (getBlue(px) * 0.114)
+      
+      lp = luminance(px)
+      lr = luminance(right)
+      lb = luminance(below)
+      
+      # If there is a large enough difference in the luminance between 
+      # your pixel and BOTH the pixels to the right and below, 
+      # then make your pixel black. 
+      if( (abs(lp - lr ) > DIFFERENCE) and (abs(lp - lb ) > DIFFERENCE) ):
+        setColor(px, black)
+      #Otherwise, make your pixel white. 
+      else:
+        setColor(px, white)
+  show(pic)      
+  return pic
   
 # improved grayscale function using weights  
 def betterBnW(pic):
