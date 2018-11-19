0d817bc515f306e4ba5dce3e39c3c6505d7ce025
kazemicode <sarieses@gmail.com>
2018-11-18 18:25:44 -0800

    Update Midterm.py
    Merged code.

--- Midterm.py
+++ Midterm.py
@@ -1,4 +1,11 @@
+# Sara Kazemi and Cody Young
+# CST 205 Midterm
+
+# CSUMB filter and VHS filter
+
+
 import random
+import datetime
 
 
 # Returns the picture given a directory
@@ -10,7 +17,10 @@ def writePict(pict,name):
   file=getMediaPath(name)
   writePictureTo(pict,file)
   
-  
+#--------------------------------------------------------------------------------#   
+# CSUMB: 
+# Create a gradient effect on the original photo, from bay blue to valley green 
+# and superimposes an image of a pixelated otter at the bottom of the photo.   
 def csumb(pic):
   # get width and height of photo to csumb-ify
   w, h = getWidth(pic), getHeight(pic)
@@ -24,14 +34,63 @@ def csumb(pic):
   #show(target)
   return target
 
+  
+#--------------------------------------------------------------------------------#    
+# VHS:
+# First under or overexposes by lightening and darkening pixels based on luminance
+# values. Then, writes the current date and time to the bottom left corner of
+# the photo. Finally, generates a picture of programmatically generated scanlines
+# and blends the two photos together.
 def vhs(pic):
   # get width and height of photo to csumb-ify
   w, h = getWidth(pic), getHeight(pic)
   # create scanlines using the height and width dimensions
+  lofi(pic)
+  setTimeAndDate(pic)
   overlay = scanlines(pic)
   target = blend(pic, overlay)
   show(target)
   return target
+
+  
+# VHS Filter 
+# Adds a vintage VHS effect to an image. Looks lo fi on purpose.
+def lofi(pic):
+  pixels = getPixels(pic)
+  for p in pixels:
+    #Desaturate pixels
+    setColor(p, makeLighter(getColor(p)))
+    #Lighten or darken each pixel based on luminance. Simulates under/overexposure.
+    luminance = (getRed(p) * 0.299 + getGreen(p) * 0.587 + getBlue(p) * 0.114)
+		
+    # Darkens pixels with lighter luminance values and lightens pixels with darker luminance values
+    if(luminance >= 163):
+      setColor(p, makeDarker(getColor(p)))
+    elif(luminance <= 93):
+      setColor(p, makeLighter(getColor(p)))
+  return pic
+    
+def setTimeAndDate(pic):
+
+  #Set height and width of time stamp relative to height and width of source picture.
+  height = getHeight(pic)
+  width = getWidth(pic)
+  #Time/date stamp height, width, font size 
+  x_position = int (width / 100)
+  y_position = height - (height / 9)
+  fontsize = (height / 10)
+		
+  #Time stamp - relative to source picture
+  time_stamp = datetime.datetime.now().strftime("%I:%M %p")
+  #Date stamp - relative to time stamp position
+  date_stamp = datetime.datetime.now().strftime("%b.%d,'%y")
+  ds_position = int(y_position + fontsize)
+		
+  #Add time and date stamps
+  addTextWithStyle(pic, x_position, y_position, time_stamp, makeStyle(mono,bold,fontsize),white)
+  addTextWithStyle(pic, x_position, ds_position, date_stamp, makeStyle(mono,bold,fontsize),white)
+  return pic   
+
   
 def blend(pic, pic2):
    w, h = getWidth(pic), getHeight(pic)
